d147399c10ed092a409ef81d03cf655212a5eaa7
KrishnanNithin <75091510+KrishnanNithin@users.noreply.github.com>
2022-05-18 18:51:25 +0400

    add player turtle

--- player.py
+++ player.py
@@ -1,7 +1,21 @@
+#imports
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.left(90)
+        self.penup()
+        self.spawn()
+        self.shape("turtle")
+    
+    def spawn(self):
+        self.goto(STARTING_POSITION)
+
+    def move(self):
+        self.forward(10)
